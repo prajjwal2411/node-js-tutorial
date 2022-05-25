579d742161296eb60b3d9d7747fd972fc72a6135
prajjwal2411 <prajjwal1997@gmail.com>
2022-05-25 23:51:16 +0530

    day-2-request-and-response-headers

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const http = require('http');
 
+//createserver creates a listener
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type', 'text/html');
+    res.write
+    (`
+        <html>
+            <head>
+                <title>My First Page</title>
+            </head>
+            <body>
+                <h1>Hello from NodeJS Server!</h1>
+            </body>
+        </html>
+    `);
+    res.end();
 });
 
 server.listen(3000);
