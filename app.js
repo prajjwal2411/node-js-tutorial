fb7acdc08e41413ea20ee006b023147128af71dc
prajjwal2411 <prajjwal1997@gmail.com>
2022-07-11 22:05:28 +0530

    day-7-express-introduction

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-const http = require('http');
-const routes = require('./routes');
+const express = require('express');
 
-//createserver creates a listener
-const server = http.createServer(routes.handler);
-console.log(routes.someText);
+const app = express();
 
-server.listen(3000);
+//use Method allows us to add a new middleware function
+app.use((req, res, next) => {
+    console.log('In the middleware');
+    next(); // Allows the request to continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+    console.log('In Other middleware');
+    res.send(`<h1>Hello from express</h1>`);
+    console.log('----')
+});
+
+//createserver creates a listener only when you are using HTTP library
+//const server = http.createServer(app);
+app.listen(3000);
