8dbd37b8da2339c67a3af0e5007ca98c2d4f78f5
prajjwal2411 <prajjwal1997@gmail.com>
2022-11-01 21:58:58 +0530

    day-8-express-routing-and-filtering-of-paths

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-//use Method allows us to add a new middleware function
-app.use((req, res, next) => {
-    console.log('In the middleware');
-    next(); // Allows the request to continue to the next middleware in line
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(adminRoutes);
+
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    console.log('In Other middleware');
-    res.send(`<h1>Hello from express</h1>`);
-    console.log('----')
+    res.status(404).send(`<h1>Page not found</h1>`);
 });
+
+// If chaining the res methods, send should be the the last method
+
+// using next after send will result in error -> as you cannot set headers after sending reponse
+
+//use Method allows us to add a new middleware function
 
 //createserver creates a listener only when you are using HTTP library
 //const server = http.createServer(app);
