e8e55b9516c4caaec9a57154539e504a6404cd94
prajjwal2411 <prajjwal1997@gmail.com>
2022-11-30 21:52:03 +0530

    ad-to-cart-functionality-complete

--- models/cart.js
+++ models/cart.js
@@ -1,10 +1,41 @@
 const fs = require('fs');
+const path = require('path');
 
+const p = path.join(
+    path.dirname(require.main.filename),
+    'data',
+    'cart.json'
+);
 module.exports = class Cart {
-    static addProduct(id) {
+    static addProduct(id, productPrice) {
         // Fetch the previous cart
-
-        // Analyze the cart => Find existing product
+        fs.readFile(p, (err, fileContent) => {
+            let cart = {products: [], totalPrice: 0}
+            if(!err) {
+                cart = JSON.parse(fileContent);
+            }
+            
+            // Analyze the cart => Find existing product
+            const existingProductIndex = cart.products.findIndex(prod => prod.id === id);
+            const existingProduct = cart.products[existingProductIndex];
+            let updatedProduct;
+            if (existingProduct) {
+                // Increase Quantity
+                updatedProduct = { ...existingProduct };
+                updatedProduct.qty = updatedProduct.qty + 1;
+                cart.products = [...cart.products];
+                cart.products[existingProductIndex] = updatedProduct;
+            } else {
+                // Add Product
+                updatedProduct = {id: id, qty: 1};
+                cart.products = [...cart.products, updatedProduct];
+            }
+            // Price Increased
+            cart.totalPrice = cart.totalPrice + +productPrice;
+            fs.writeFile(p, JSON.stringify(cart), (err) => {
+                console.log(err);
+            })
+        });
         // Add new product/ increase quantity
     }
 
